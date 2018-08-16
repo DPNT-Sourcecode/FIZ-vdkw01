a7be71c845c09268c2f8ef900ca29f57212bc57e
sbx_user1060 <sbx_user1060@ip-10-129-198-249.eu-west-2.compute.internal>
2018-08-16 21:21:22 +0000

    Thu Aug 16 21:21:22 UTC 2018

--- src/main/scala/befaster/solutions/FIZ/FizzBuzz.scala
+++ src/main/scala/befaster/solutions/FIZ/FizzBuzz.scala
@@ -2,17 +2,14 @@ package befaster.solutions.FIZ
 
 object FizzBuzz {
   def fizzBuzz(number: Int): String = number match {
-    case n if n % 15 == 0 => "fizz buzz"
-    case n if containsThree(n) => "fizz"
-    case n if n % 5 == 0 => "buzz"
+    case n if n.divisibleBy(15) => "fizz buzz"
+    case n if n.divisibleBy(3) || n.contains(3) => "fizz"
+    case n if n.divisibleBy(5) => "buzz"
     case n => n.toString
   }
 
-  private def divisibleBy(number: Int)
-
-  private def containsThree(number: Int): Boolean = number.toString.contains('3')
-
   implicit class FizzBuzzNumber(number: Int) {
     def divisibleBy(divisor: Int): Boolean = number % divisor == 0
+    def contains(digit: Int): Boolean = number.toString.contains(digit.toString)
   }
 }
