4a7443bd5b41f72825c0fd86a470c27854eb47c6
sbx_user1060 <sbx_user1060@ip-10-129-198-249.eu-west-2.compute.internal>
2018-08-16 20:47:49 +0000

    Thu Aug 16 20:47:49 UTC 2018

--- src/main/scala/befaster/solutions/FIZ/FizzBuzz.scala
+++ src/main/scala/befaster/solutions/FIZ/FizzBuzz.scala
@@ -1,7 +1,5 @@
 package befaster.solutions.FIZ
 
-import befaster.runner.SolutionNotImplementedException
-
 object FizzBuzz {
-  def fizzBuzz(number: Int): String = throw new SolutionNotImplementedException()
+  def fizzBuzz(number: Int): String = "fizz"
 }
--- src/test/scala/befaster/solutions/FIZ/FizzBuzzTest.scala
+++ src/test/scala/befaster/solutions/FIZ/FizzBuzzTest.scala
@@ -1,5 +1,17 @@
 package befaster.solutions.FIZ
 
-class FizzBuzzTest {
+import org.scalatest.{FlatSpec, Matchers}
 
+class FizzBuzzTest extends FlatSpec with Matchers {
+
+  "The fizz buzz function" should "return 'fizz' when the provided number is a multiple of 3" in {
+    FizzBuzz.fizzBuzz(3) should equal ("fizz")
+    FizzBuzz.fizzBuzz(6) should equal ("fizz")
+    FizzBuzz.fizzBuzz(9) should equal ("fizz")
+    FizzBuzz.fizzBuzz(18) should equal ("fizz")
+  }
+
+  it should "return 'buzz' when the number is a multiple of 5" in {
+    FizzBuzz.fizzBuzz(5) should equal ("buzz")
+  }
 }
