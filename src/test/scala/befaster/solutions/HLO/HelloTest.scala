11e76532649066528a96056cac9b13248fdb3352
sbx_user1060 <sbx_user1060@ip-10-129-198-249.eu-west-2.compute.internal>
2018-08-16 20:41:24 +0000

    Thu Aug 16 20:41:24 UTC 2018

--- src/test/scala/befaster/solutions/HLO/HelloTest.scala
+++ src/test/scala/befaster/solutions/HLO/HelloTest.scala
@@ -4,11 +4,11 @@ import org.scalatest.{FlatSpec, Matchers}
 
 class HelloTest extends FlatSpec with Matchers {
 
-  "The hello class" should "return 'Hello, World!' when given the name Craftsman" in {
-    Hello.hello("Craftsman") should equal ("Hello, World!")
+  "The hello class" should "return 'Hello, World!' when given the name World" in {
+    Hello.hello("World") should equal ("Hello, World!")
   }
 
-  it should "return 'Hello, World!' regardless of user input" in {
-    Hello.hello("Mr. X") should equal ("Hello, World!")
+  it should "return a greeting to the provided name" in {
+    Hello.hello("Mr. X") should equal ("Hello, Mr. X!")
   }
 }
