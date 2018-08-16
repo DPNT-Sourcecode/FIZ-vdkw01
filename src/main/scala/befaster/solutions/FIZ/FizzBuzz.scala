c26951757fb80c8bd68fc301014163f91d60c969
sbx_user1060 <sbx_user1060@ip-10-129-198-249.eu-west-2.compute.internal>
2018-08-16 21:15:22 +0000

    Thu Aug 16 21:15:22 UTC 2018

--- src/main/scala/befaster/solutions/FIZ/FizzBuzz.scala
+++ src/main/scala/befaster/solutions/FIZ/FizzBuzz.scala
@@ -3,8 +3,12 @@ package befaster.solutions.FIZ
 object FizzBuzz {
   def fizzBuzz(number: Int): String = number match {
     case n if n % 15 == 0 => "fizz buzz"
-    case n if n % 3 == 0 => "fizz"
+    case n if containsThree(n) => "fizz"
     case n if n % 5 == 0 => "buzz"
     case n => n.toString
   }
+
+  private def divisibleBy(number: Int)
+
+  private def containsThree(number: Int): Boolean = number.toString.contains('3')
 }
